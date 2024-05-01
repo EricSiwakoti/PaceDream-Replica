79c12f83f8a642e4dabcb59ee816a8c0b304a1ea
Eric <bonderic44@gmail.com>
2024-05-01 16:18:20 +0545

    feat : card component created

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,16 +1,26 @@
 import React, { ReactNode } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface CardProps {
   className: string;
   children: ReactNode;
+  title: string;
+  body: string;
+  icon: IconProp;
 }
 
 const Card = (props: CardProps) => {
   return (
     <div
-      className={`card m-0 shadow-md rounded-lg overflow-hidden bg-white ${props.className}`}
+      className={`relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl p-3 w-1/4 ${props.className}`}
     >
-      {props.children}
+      <p>{props.title}</p>
+      <div className="flex items-center space-x-3 text-black">
+        <p className="font-bold text-2xl">{props.body}</p>
+        <FontAwesomeIcon icon={props.icon} />
+      </div>
+      <p className="text-xs py-1">{props.children}</p>
     </div>
   );
 };
